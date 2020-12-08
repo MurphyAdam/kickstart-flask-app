05e5859b8fbde2f2aa5269fd2c498f03a191b483
Adam Clark <elm.majidi@gmail.com>
2020-12-08 22:20:34 +0100

    added entry_points

--- setup.py
+++ setup.py
@@ -9,16 +9,21 @@ except IOError:
 
 setup(
     name="kickstart-flask-app",
-    version="1.0.1",
+    version="1.0.4",
     description="Generate a new Flask app project.",
     license="MIT",
     author="Majdi Mahfoud",
     author_email="elm.majidi@gmail.com",
+    url="https://github.com/MurphyAdam/kickstart-flask-app",
     keywords=['Flask', 'template', 'generate'],
     requires=['Flask', 'gunicorn'],
     packages=find_packages(),
     install_requires=[],
     long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'kickstart-flask-app = kickstart_flask_app.KickstartFlaskApp:main'],
+    },
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.8",
