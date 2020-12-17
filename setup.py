bcecfee71e1142a6313ee525bbb35fa3e219b4ac
Adam Clark <elm.majidi@gmail.com>
2020-12-17 13:46:27 +0100

    new version

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="kickstart_flask_app",
-    version="1.1.3",
+    version="1.1.4",
     description="Generate a new Flask app project.",
     license="MIT",
     author="Majdi Mahfoud",
     author_email="elm.majidi@gmail.com",
     url="https://github.com/MurphyAdam/kickstart-flask-app",
     keywords=['Flask', 'template', 'generate'],
-    py_modules=["kickstart_flask_app"],
     packages=find_packages(),
     install_requires=[],
     long_description='Please refer to README for docs',
